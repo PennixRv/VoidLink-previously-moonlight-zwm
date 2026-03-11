d5772221c9572b4ee03b58de7f3e03064acd6d34
penn <pennaliflake@gmail.com>
2026-03-11 15:47:01 +0800

    tvOS: bump deployment target to 15 and fix AboutViewController

--- VoidLink/AboutViewController.swift
+++ VoidLink/AboutViewController.swift
@@ -28,11 +28,13 @@ import SwiftUI
             hostingVC.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
         ])
 
+        #if !os(tvOS)
         // 🎯 设置弹窗大小
         preferredContentSize = CGSize(width: 530, height: 430)
 
         // ⚠️ 一定要设置以下样式才会生效 preferredContentSize
         modalPresentationStyle = .formSheet
+        #endif
     }
 }
 
